4be9ef9ce3940447b92be13cfcbd2b9e863b2f2e
[CLOUD4] <dbslxl11@gmail.com>
2024-09-30 17:21:46 +0900

    프로젝트 세팅

---
+++ shoux-kream/src/main/java/com/shoux_kream/ShouxKreamApplication.java
@@ -0,0 +1,13 @@
+package com.shoux_kream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShouxKreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShouxKreamApplication.class, args);
+	}
+
+}
---
+++ shoux-kream/src/test/java/com/shoux_kream/ShouxKreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoux_kream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShouxKreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
