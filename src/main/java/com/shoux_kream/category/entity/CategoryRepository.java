e28d4d0dc53b41a0c8093ee65cbbab81154823e8
[CLOUD4] 한선규 <sunkyu821@gmail.com>
2024-10-04 01:36:03 +0900

    feat : CategoryRepository 작성

---
+++ src/main/java/com/shoux_kream/category/entity/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.shoux_kream.category.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name); //카테고리 검색 시 해당 데이터가 없을 때 Optional 반환
+}
