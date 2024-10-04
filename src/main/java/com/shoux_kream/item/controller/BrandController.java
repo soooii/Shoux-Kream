d326cc87eea83a57c8047562a7dfc1528860a3e2
[CLOUD4] 박유찬 <pychan0306@gmail.com>
2024-10-04 23:08:56 +0900

    Feat: GetMapping RestController -> Controller

--- src/main/java/com/shoux_kream/item/controller/BrandController.java
+++ src/main/java/com/shoux_kream/item/controller/BrandController.java
@@ -7,11 +7,12 @@ import com.shoux_kream.item.service.BrandService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/brands")
 public class BrandController {
 
@@ -23,18 +24,16 @@ public class BrandController {
 
     // 모든 인증된 사용자 접근 가능 - 주어진 id에 해당하는 브랜드 정보를 조회하고 반환
     @GetMapping("/{brandId}")
-    public ResponseEntity<BrandResponse> getBrandInfo(@PathVariable Long brandId) {
+    public String getBrandInfo(@PathVariable Long brandId) {
         BrandResponse findBrandById = brandService.findById(brandId);
-
-        return ResponseEntity.ok(findBrandById);
+        return "";
     }
 
     // 모든 인증된 사용자 접근 가능 - 모든 브랜드의 목록을 조회하여 응답
     @GetMapping
-    public ResponseEntity<BrandsGetResponse> getBrandsInfo() {
+    public String getBrandsInfo() {
         List<BrandResponse> brandList = brandService.findAll();
-
-        return ResponseEntity.ok(new BrandsGetResponse(brandList.size(), brandList));
+        return "";
     }
 
     // 관리자 권한 필요 - 새로운 브랜드를 등록하고, 등록된 브랜드 정보를 반환
@@ -42,7 +41,6 @@ public class BrandController {
     @PostMapping
     public ResponseEntity<BrandResponse> saveBrand(@RequestBody BrandSaveRequest brandSaveRequest) {
         BrandResponse savedBrandResponse = brandService.save(brandSaveRequest.name());
-
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedBrandResponse);
     }
