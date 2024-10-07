4c09b3ada3dc0b10c9689ea1246f98886d7ee8c6
Sur0n <skyzephyr333@gmail.com>
2024-10-07 15:24:39 +0900

    feat : dto 필드, builder 추가 requestDto toEntity 추가

--- src/main/java/com/shoux_kream/checkout/dto/CheckOutResponseDto.java
+++ src/main/java/com/shoux_kream/checkout/dto/CheckOutResponseDto.java
@@ -3,14 +3,24 @@ package com.shoux_kream.checkout.dto;
 import com.shoux_kream.cart.entity.Cart;
 import com.shoux_kream.checkout.entity.CheckOut;
 import com.shoux_kream.checkout.entity.Receipt;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
 @NoArgsConstructor
 public class CheckOutResponseDto {
 
-    public CheckOut toEntity(Receipt receipt, List<Cart> carts){
-        return CheckOut.builder().receipt(receipt).carts(carts).build();
+    private Long Id;
+    List<Cart> carts;
+    Receipt receipt;
+
+    @Builder
+    public CheckOutResponseDto(Long id, List<Cart> carts, Receipt receipt) {
+        Id = id;
+        this.carts = carts;
+        this.receipt = receipt;
     }
 }
