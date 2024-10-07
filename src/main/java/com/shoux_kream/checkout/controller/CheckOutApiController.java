bae351daafeeb2dd8f6cceb90afafb8df657784a
Sur0n <skyzephyr333@gmail.com>
2024-10-07 14:01:12 +0900

    feat : Api Controller 어노테이션 rest변경

--- src/main/java/com/shoux_kream/checkout/controller/CheckOutApiController.java
+++ src/main/java/com/shoux_kream/checkout/controller/CheckOutApiController.java
@@ -11,8 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/checkout")
 @RequiredArgsConstructor
 public class CheckOutApiController {
