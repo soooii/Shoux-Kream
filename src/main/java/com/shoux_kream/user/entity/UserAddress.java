1446a715ad0f1ae8eb4bf5c3c65be7a18c597357
[cloud4] 안송희 <songhee9327@gmail.com>
2024-10-09 22:50:26 +0900

    feat : 유저 address 클래스 추가

---
+++ src/main/java/com/shoux_kream/user/entity/UserAddress.java
@@ -0,0 +1,44 @@
+package com.shoux_kream.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 수령인 이름
+    @Column(name = "recipient_name", nullable = false)
+    private String recipientName;
+
+    // 수령인 전화번호
+    @Column(name = "recipient_phone", nullable = false)
+    private String recipientPhone;
+
+    // 우편번호
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+
+    //주소
+    @Column(name = "address1", nullable = false)
+    private String address1;
+
+    //상세주소
+    @Column(name = "address2")
+    private String address2;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
+
