346bbeac39e61c640e71b60e40a7666addd6282b
Sur0n <skyzephyr333@gmail.com>
2024-10-13 04:48:12 +0900

    feat : 사용자 주소 정보 불러오기 기능 추가

--- src/main/java/com/shoux_kream/checkout/entity/CheckOut.java
+++ src/main/java/com/shoux_kream/checkout/entity/CheckOut.java
@@ -1,8 +1,11 @@
 package com.shoux_kream.checkout.entity;
 
 import com.shoux_kream.cart.entity.Cart;
+import com.shoux_kream.checkout.dto.CheckOutResponseDto;
 import com.shoux_kream.timestamp.BaseEntity;
+import com.shoux_kream.user.dto.response.UserAddressDto;
 import com.shoux_kream.user.entity.User;
+import com.shoux_kream.user.entity.UserAddress;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,7 +29,7 @@ public class CheckOut extends BaseEntity {
 
     @ManyToOne
     @JoinColumn(name = "address_id")
-    private Address address;
+    private UserAddress address;
 
     private String request;
 
@@ -35,13 +38,22 @@ public class CheckOut extends BaseEntity {
     private List<CheckOutItem> checkOutItems;
 
     @Builder
-    public CheckOut(List<CheckOutItem> checkOutItems, String request, Address address, User user, int totalPrice, String summaryTitle, Long id) {
+    public CheckOut(List<CheckOutItem> checkOutItems, String request, UserAddress address, User user, int totalPrice, String summaryTitle) {
         this.checkOutItems = checkOutItems;
         this.request = request;
         this.address = address;
         this.user = user;
         this.totalPrice = totalPrice;
         this.summaryTitle = summaryTitle;
-        this.id = id;
+    }
+
+    public CheckOutResponseDto toDto() {
+        return CheckOutResponseDto.builder()
+                .id(this.id)
+                .summaryTitle(this.summaryTitle)
+                .totalPrice(this.totalPrice)
+                .address(UserAddressDto.builder().userAddress(this.address).build())
+                .request(this.request)
+                .build();
     }
 }
