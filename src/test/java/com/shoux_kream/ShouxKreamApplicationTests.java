1259d8c696b657449b83e69cd69bf2c884e673af
Sur0n <skyzephyr333@gmail.com>
2024-10-17 02:16:04 +0900

    feat : test용 환경변수 주입 설정

--- src/test/java/com/shoux_kream/ShouxKreamApplicationTests.java
+++ src/test/java/com/shoux_kream/ShouxKreamApplicationTests.java
@@ -2,8 +2,20 @@ package com.shoux_kream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
+//applcation test 프로필 가져오기
 @SpringBootTest
+@TestPropertySource(properties = {
+		"S3_SHOUXKREAM_ACCESS_KEY=test",
+		"S3_SHOUXKREAM_SECRET_KEY=test",
+		"S3_SHOUXKREAM_BUCKET=test",
+		"JWT_SECRET_KEY=ZB67NHMI84Y20zQVFSf8nZfChJoG6rZ+16x8HgzWuw8="
+})
+@Import(MockS3Config.class)
+@ActiveProfiles("test")
 class ShouxKreamApplicationTests {
 
 	@Test
