eb613bd09590470f57b5e0207d826149de995f6e
[cloud4] 안송희 <songhee9327@gmail.com>
2024-10-17 14:32:32 +0900

    refactor: 회원정보 수정 api 변경, 비밀번호 불일치 Exception Handler 추가

--- src/main/java/com/shoux_kream/user/controller/UserController.java
+++ src/main/java/com/shoux_kream/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.shoux_kream.user.controller;
 
+import com.shoux_kream.exception.InvalidPasswordException;
+import com.shoux_kream.user.dto.request.AccountRequest;
 import com.shoux_kream.user.dto.request.UserRequest;
 import com.shoux_kream.user.dto.response.UserAddressDto;
 import com.shoux_kream.user.dto.response.UserResponse;
@@ -31,28 +33,33 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
     }
 
-    //계정 관리 페이지
-    @PatchMapping("/me/profile")
-    public ResponseEntity<UserResponse> updateProfile(@AuthenticationPrincipal User principal, @RequestBody UserRequest userRequest) {
-        log.info(principal.getUsername());
-        UserResponse userResponse = userService.updateProfile(principal.getUsername(), userRequest);
-        return ResponseEntity.ok(userResponse);
-    }
-
-    //회원 탈퇴
-    @DeleteMapping("/me")
-    public ResponseEntity<String> delete(@AuthenticationPrincipal User principal) {
-        userService.deleteUser(principal.getUsername());
-        return ResponseEntity.ok("탈퇴가 완료되었습니다.");
-    }
-
-
-    //마이페이지
-    @GetMapping({"/me"})
+    //회원정보 불러오기
+    @GetMapping("/me")
     public ResponseEntity<UserResponse> myPage() {
         UserResponse userResponse = userService.getUser();
         return ResponseEntity.ok(userResponse);
     }
+
+    //입력한 기존 비밀번호가 db 비밀번호와 다를 때 발생하는 Exception
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ResponseEntity<String> handleInvalidPasswordException(InvalidPasswordException ex) {
+        return ResponseEntity.status(418).body(ex.getMessage());
+    }
+
+    //회원정보 수정
+    @PatchMapping({"/me"})
+    public ResponseEntity<String> updateProfile(@RequestBody AccountRequest accountRequest) {
+        userService.updateProfile(accountRequest);
+        return ResponseEntity.ok("수정이 완료되었습니다.");
+    }
+
+    //회원 탈퇴
+    @DeleteMapping("/me")
+    public ResponseEntity<String> delete() {
+        userService.deleteUser();
+        return ResponseEntity.ok("탈퇴가 완료되었습니다.");
+    }
+
 
     @GetMapping("/userAddress")
     public ResponseEntity<List<UserAddressDto>> getUserAddress(@AuthenticationPrincipal User principal){
