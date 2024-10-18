4fa555090163eddefb08789f8dfd92370603bef0
[CLOUD4] 한선규 <sunkyu821@gmail.com>
2024-10-18 16:49:28 +0900

    feat : category 수정관련 부분 코드 추가,수정

--- src/main/java/com/shoux_kream/config/SecurityPath.java
+++ src/main/java/com/shoux_kream/config/SecurityPath.java
@@ -10,12 +10,14 @@ public class SecurityPath {
             "/item/item-list",
             "/item/item-detail/{id}",
             "/item/edit/{id}",
-            "/item/item-add"
+            "/item/item-add",
+            "/category/category-add"
     };
     public static final String[] ONLY_POST_WHITELIST = {
             "/api/users/login",
             "/api/users/signup",
             "/api/users/refresh",
-            "/item/item-add"
+            "/item/item-add",
+            "/category/category-add"
     };
 }
