6fe340650b0635e339cc8ec30fdda38f47c77fba
Sur0n <skyzephyr333@gmail.com>
2024-10-18 12:22:08 +0900

    setting : record => class 역직렬화 테스트

--- src/main/java/com/shoux_kream/item/dto/response/ItemResponse.java
+++ src/main/java/com/shoux_kream/item/dto/response/ItemResponse.java
@@ -3,35 +3,53 @@ package com.shoux_kream.item.dto.response;
 import com.shoux_kream.category.entity.Category;
 import com.shoux_kream.item.entity.Item;
 import com.shoux_kream.item.entity.ItemInfo;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-public record ItemResponse(
-        Long id,
+@Data
+@NoArgsConstructor
+public class ItemResponse{
+        Long id;
 //        BrandResponse brand,
-        String title,
+        String title;
 //        Category category,
-        String manufacturer,
-        String shortDescription,
-        String detailDescription,
-        String imageKey,
-        Integer inventory,
-        Integer price,
-        List<String> searchKeywords
-) {
+        String manufacturer;
+        String shortDescription;
+        String detailDescription;
+        String imageKey;
+        Integer inventory;
+        Integer price;
+        List<String> searchKeywords;
+
+        @Builder
+        public ItemResponse(Long id, String title, String manufacturer, String shortDescription, String detailDescription, String imageKey, Integer inventory, Integer price, List<String> searchKeywords) {
+            this.id = id;
+            this.title = title;
+            this.manufacturer = manufacturer;
+            this.shortDescription = shortDescription;
+            this.detailDescription = detailDescription;
+            this.imageKey = imageKey;
+            this.inventory = inventory;
+            this.price = price;
+            this.searchKeywords = searchKeywords;
+        }
+
     public static ItemResponse fromEntity(Item item) {
-        return new ItemResponse(
-                item.getId(),
-//                BrandResponse.fromEntity(item.getBrand()),
-                item.getTitle(),
-//                item.getCategory(),
-                item.getManufacturer(),
-                item.getShortDescription(),
-                item.getDetailDescription(),
-                item.getImageKey(),
-                item.getInventory(),
-                item.getPrice(),
-                item.getSearchKeywords()
-        );
+        return ItemResponse.builder()
+                .id(item.getId())
+//                .brandResponse(BrandResponse.fromEntity(item.getBrand()))
+                .title(item.getTitle())
+//                .category(item.getCategory())
+                .manufacturer(item.getManufacturer())
+                .shortDescription(item.getShortDescription())
+                .detailDescription(item.getDetailDescription())
+                .imageKey(item.getImageKey())
+                .inventory(item.getInventory())
+                .price(item.getPrice())
+                .searchKeywords(item.getSearchKeywords())
+                .build();
     }
 }
