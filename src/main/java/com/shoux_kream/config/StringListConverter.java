5623d0e3030d3f76b19e65c94e282082cd77f948
Sur0n <skyzephyr333@gmail.com>
2024-10-21 13:56:46 +0900

    refactor : List<String> 형태 응답을 위한 직렬화 처리를 위한 리팩토링

--- src/main/java/com/shoux_kream/config/StringListConverter.java
+++ src/main/java/com/shoux_kream/config/StringListConverter.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Converter
 public class StringListConverter implements AttributeConverter<List<String>, String> {
@@ -13,20 +15,19 @@ public class StringListConverter implements AttributeConverter<List<String>, Str
     private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
-    public String convertToDatabaseColumn(List<String> dataList) {
-        try {
-            return mapper.writeValueAsString(dataList);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+    public String convertToDatabaseColumn(List<String> attribute) {
+        if (attribute == null || attribute.isEmpty()) {
+            return "";
         }
+        return String.join(",", attribute);  // List<String>을 콤마로 연결된 문자열로 변환
     }
 
     @Override
-    public List<String> convertToEntityAttribute(String data) {
-        try {
-            return mapper.readValue(data, List.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
+    public List<String> convertToEntityAttribute(String dbData) {
+        if (dbData == null || dbData.isEmpty()) {
+            return List.of();  // 비어있는 문자열일 경우 빈 리스트 반환
         }
+        return Arrays.stream(dbData.split(","))
+                .collect(Collectors.toList());  // 콤마로 구분된 문자열을 List<String>으로 변환
     }
 }
