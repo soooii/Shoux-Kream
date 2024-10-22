63993a385c97473f5e927fa88187699dd0f567df
[CLOUD4] 한현 <gusgus1467@naver.com>
2024-10-22 16:29:42 +0900

    feat : 판매 등록 정산 페이지 추가

--- src/main/java/com/shoux_kream/sale/controller/SaleViewController.java
+++ src/main/java/com/shoux_kream/sale/controller/SaleViewController.java
@@ -8,23 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SaleViewController {
 
-    // 판매 입찰하는 과정 -> 판매 등록 후 수정 페이지 함께 사용
+    // 판매 입찰하는 과정
     @GetMapping("/sale")
     public String getSale() {
         return "sale/sale";
     }
 
-    // TODO 판매 입찰 완료 페이지
+    // 판매 입찰 정산 페이지
     @GetMapping("/sale/complete")
     public String getSaleComplete() {
         return "sale/salecomplete";
     }
 
     // TODO 판매 입찰 후 마이페이지에서 판매 내역 확인
-    @GetMapping("/selling")
-    public String getSelling() {
-        return "sale/selling";
-    }
+//    @GetMapping("/selling")
+//    public String getSelling() {
+//        return "sale/selling";
+//    }
 
     // TODO 마이페이지 판매 내역 -> 상세 내역
 
