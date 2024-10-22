fce3fdb0cddc09b61a9f50cb00f731694357b954
[cloud4] 안송희 <songhee9327@gmail.com>
2024-10-22 15:44:34 +0900

    remove : 불필요 import 삭제

--- src/main/java/com/shoux_kream/user/controller/JwtController.java
+++ src/main/java/com/shoux_kream/user/controller/JwtController.java
@@ -18,13 +18,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
