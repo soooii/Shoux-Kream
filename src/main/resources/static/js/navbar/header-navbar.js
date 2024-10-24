35a7d4a5706a4aea189738caf3d5e5a925c2ed85
[CLOUD4] <dbslxl11@gmail.com>
2024-10-24 18:18:33 +0900

    refactor : 우측 상단 헤더 버튼 반짝이는거 수정

--- src/main/resources/static/js/navbar/header-navbar.js
+++ src/main/resources/static/js/navbar/header-navbar.js
@@ -7,9 +7,9 @@ async function navbarCheckAdmin() {
   const token = sessionStorage.getItem("accessToken");
   // 우선 토큰 존재 여부 확인
   if (!token) {
-    document.getElementById('adminButton').style.display = 'none';
-    document.getElementById('cartButton').style.display = 'none';
-    document.getElementById('myButton').style.display = 'none';
+//    document.getElementById('adminButton').style.display = 'none';
+//    document.getElementById('cartButton').style.display = 'none';
+//    document.getElementById('myButton').style.display = 'none';
   } else {
     // 관리자 토큰 여부 확인
       const res = await fetch("/api/admin/check", {
@@ -20,12 +20,14 @@ async function navbarCheckAdmin() {
 
       const result = await res.text();
       if (result === "ADMIN") {
-        document.getElementById('adminButton').style.display = 'none';
-        document.getElementById('cartButton').style.display = 'none';
-        document.getElementById('myButton').style.display = 'none';
+//        document.getElementById('adminButton').style.display = 'none';
+//        document.getElementById('cartButton').style.display = 'none';
+//        document.getElementById('myButton').style.display = 'none';
         return;
       } else {
-        document.getElementById('adminButton').style.display = 'none';
+//        document.getElementById('adminButton').style.display = 'none';
+        document.getElementById('cartButton').style.display = 'inline';
+        document.getElementById('myButton').style.display = 'inline';
       }
   }
 };
